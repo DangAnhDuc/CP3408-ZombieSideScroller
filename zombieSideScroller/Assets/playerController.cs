33295c0334a8c51fc4a8856537cef89087dbbfd8
topprono123 <topprono123@gmail.com>
2017-09-21 18:33:57 +0800

    Set up animation run and walk of character

--- zombieSideScroller/Assets/playerController.cs
+++ zombieSideScroller/Assets/playerController.cs
@@ -6,6 +6,7 @@ public class playerController : MonoBehaviour
 {
 
     public float runSpeed;
+    public float walkSpeed;
 
     Rigidbody myRB;
     Animator myAnim;
@@ -30,7 +31,18 @@ public class playerController : MonoBehaviour
         float move = Input.GetAxisRaw("Horizontal");
         myAnim.SetFloat("speed", Mathf.Abs(move));
 
-        myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+        float sneaking = Input.GetAxisRaw("Fire3");
+        myAnim.SetFloat("sneaking", sneaking);
+
+        if (sneaking > 0)
+        {
+            myRB.velocity = new Vector3(move * walkSpeed, myRB.velocity.y, 0);
+        }
+        else
+        {
+            myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+
+        }
 
         if (move > 0 && !facingRight) Flip();
         else if (move < 0 && facingRight) Flip();
